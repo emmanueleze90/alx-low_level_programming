4aec12f4ba211ef9801a3b14f541c2e99e37f6b1
emmanueleze90 <62996194+emmanueleze90@users.noreply.github.com>
2022-10-17 16:46:54 -0700

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		char a;
+	char a;
 
-			for (a = 'a'; a <= 'z'; a++)
-					{
-								if (a == 'e' || a == 'q')
-												continue;
-										putchar(a);
-											}
-				putchar('\n');
-					return (0);
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e' || a == 'q')
+			continue;
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
 }
