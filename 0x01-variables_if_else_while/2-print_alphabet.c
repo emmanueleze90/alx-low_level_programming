99a34ac459850a948a82b5e22d5f753d09e6a428
emmanueleze90 <62996194+emmanueleze90@users.noreply.github.com>
2022-10-17 16:44:31 -0700

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'The program description'
- *     *
- *      * Return: Always 0 (success)
- *       */
+ * main - Entry point
+ *
+ * Description: 'The program description'
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		int a;
+	int a;
 
-			for (a = 'a'; a <= 'z'; a++)
-						putchar(a);
-				putchar('\n');
-					return (0);
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
 }
