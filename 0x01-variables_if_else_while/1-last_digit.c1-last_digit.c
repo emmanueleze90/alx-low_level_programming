a389eaba7af48aef6deae7a41e71ea8a5f3ec8aa
emmanueleze90 <62996194+emmanueleze90@users.noreply.github.com>
2022-10-17 16:43:44 -0700

    Update 1-last_digit.c1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c1-last_digit.c
@@ -2,66 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'The program description
- *     *
- *      * Return: Always 0 (success)
- *      */
+ * main - Entry point
+ *
+ * Description: 'The program description
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
-		int n;
-			int test;
+	int n;
+	int test;
 
-				srand(time(0));
-					n = rand() - RAND_MAX / 2;
-						test = n % 10;
-							if (test > 5)
-									{
-												printf("Last digit of %d is %d and is greater than 5\n", n, test);
-													} else if (test == 0)
-															{
-																		printf("Last digit of %d is %d and is 0\n", n, test);
-																			} else if (test < 6 && test != 0)
-																					{
-																								printf("Last digit of %d is %d and is less than 6 and not 0\n", n, test);
-																									}
-																				return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	test = n % 10;
+	if (test > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, test);
+	} else if (test == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, test);
+	} else if (test < 6 && test != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, test);
+	}
+	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- *  * main - Entry point
- *   *
- *    * Description: 'The program description
- *     *
- *      * Return: Always 0 (success)
- *      */
-int main(void)
-{
-		int n;
-			int test;
-
-				srand(time(0));
-					n = rand() - RAND_MAX / 2;
-						test = n % 10;
-							if (test > 5)
-									{
-												printf("Last digit of %d is %d and is greater than 5\n", n, test);
-													} else if (test == 0)
-															{
-																		printf("Last digit of %d is %d and is 0\n", n, test);
-																			} else if (test < 6 && test != 0)
-																					{
-																								printf("Last digit of %d is %d and is less than 6 and not 0\n", n, test);
-																									}
-																				return (0);
-}
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
