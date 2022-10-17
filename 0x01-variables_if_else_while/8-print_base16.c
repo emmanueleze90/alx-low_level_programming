715a790d37e557a8b835ae229e9c73ebd2a0d47b
emmanueleze90 <62996194+emmanueleze90@users.noreply.github.com>
2022-10-17 16:52:51 -0700

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,31 +1,16 @@
-a#include <stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		int i;
-			char hexvalue[] = "0123456789abcdef";
+	int i;
+	char hexvalue[] = "0123456789abcdef";
 
-				for (i = 0; i < 16; i++)
-							putchar(hexvalue[i]);
-					putchar('\n');
-						return (0);
-}#include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
-int main(void)
-{
-		int i;
-			char hexvalue[] = "0123456789abcdef";
-
-				for (i = 0; i < 16; i++)
-							putchar(hexvalue[i]);
-					putchar('\n');
-						return (0);
+	for (i = 0; i < 16; i++)
+		putchar(hexvalue[i]);
+	putchar('\n');
+	return (0);
 }
