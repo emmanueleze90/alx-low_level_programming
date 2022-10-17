4cb9a81dfbc81a5b52a24cc7584f44cbb7807dc1
emmanueleze90 <emmanueleze@udobatech.com>
2022-10-17 16:36:17 -0700

    Winner

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: 'The program description'
+ *     *
+ *      * Return: Always 0 (success)
+ *       */
+int main(void)
+{
+		int a;
+			int b;
+
+				for (a = 'a'; a <= 'z'; a++)
+							putchar(a);
+					for (b = 'A'; b <= 'Z'; b++)
+								putchar(b);
+						putchar('\n');
+							return (0);
+}#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: 'The program description'
+ *     *
+ *      * Return: Always 0 (success)
+ *       */
+int main(void)
+{
+		int a;
+			int b;
+
+				for (a = 'a'; a <= 'z'; a++)
+							putchar(a);
+					for (b = 'A'; b <= 'Z'; b++)
+								putchar(b);
+						putchar('\n');
+							return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		char a;
+
+			for (a = 'a'; a <= 'z'; a++)
+					{
+								if (a == 'e' || a == 'q')
+												continue;
+										putchar(a);
+											}
+				putchar('\n');
+					return (0);
+}
