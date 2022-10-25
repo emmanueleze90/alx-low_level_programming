5077c674d848547a8ed34ff1efcbaafe6140ef6d
emmanueleze90 <emmanueleze@udobatech.com>
2022-10-24 20:33:35 -0700

    Winner

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *reset_to_98 - resets the value of a given integer to 98
- *@n: parameter
- *Return : nothing
- */
-
+*reset_to_98 - resets the value of a given integer to 98
+*@n: parameter
+*Return : nothing
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
